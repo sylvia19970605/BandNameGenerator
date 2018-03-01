01641c5f5ef514346054814d9383e19f321e70a7
sylvia.wang55 <sylvia.wang55@titansoft.com.sg>
2018-03-01 09:45:38 +0800

    完成版。

---
+++ Band Name Generator/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Band_Name_Generator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string BandName = textBox1.Text;    //從textBox1中取得字串
+            string lower = BandName.ToLower();    //將字串中的所有字元轉換成小寫
+            var split = lower.ToCharArray();    //切割字串中的字元成陣列
+
+            if (split[0] != split[split.Length-1])    //若字首不等於字尾
+            {
+                label1.Text = "The " + BandName;   //於label1顯示
+            }
+            else
+            {
+                label1.Text = BandName.Substring(0, BandName.Length - 1) + lower.ToLower();    //先移除BandName字串最後一個字元，再加上一次BandName字串，顯示於label1
+            }
+        }
+    }
+}
---
+++ Unit Test Band Name Generator/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Unit Test Band Name Generator")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Unit Test Band Name Generator")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d1a77a08-fb5d-4298-84ba-968ceb7ab770")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
